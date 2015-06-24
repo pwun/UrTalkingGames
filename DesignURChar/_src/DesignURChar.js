f4baeeefe66424e0e24555fe4d333940a0a80269
FelixSellmair <felix.sellmair@gmail.com>
2015-06-24 15:55:15 +0200

    design update

--- DesignURChar/_src/DesignURChar.js
+++ DesignURChar/_src/DesignURChar.js
@@ -3,13 +3,43 @@ var DesignURChar = (function() {
         canvas, 
         
     init = function() {
-        console.log("chardesign init");
-        canvas = document.getElementById('canvas');
+        _initCanvas();
+        _initMenu();
+        
+         
+    },
+    _initCanvas = function() {
+    canvas = document.getElementById('canvas');
         
         canvas.height = (canvas.width/16)*9;
-        console.log(canvas.width);
-        console.log(canvas.height);
-         
+      },
+    _initMenu = function() {
+    
+    // function to trigger animation
+$('.button').click(function() {
+  
+  // check if the menu-items are hidden behind the button
+  if ($('.menu__list').hasClass('hidden')) {
+    // add class to make the menu-items drop down
+    $('.item1').addClass('list--animation');
+    // the following items trigger the animation after a certain delay
+    $('.item2').addClass('list--animation--delay1');
+    $('.item3').addClass('list--animation--delay2');
+    // remove the hidden class from the ul container to show that the items are not hidden anymore
+    $('.menu__list').removeClass('hidden');
+  }
+  else {
+    // remove class to make the menu-items disappear
+    $('.item1').removeClass('list--animation');
+    $('.item2').removeClass('list--animation--delay1');
+    $('.item3').removeClass('list--animation--delay2');
+    // add the hidden class to the ul container to show that the items are now hidden again 
+    $('.menu__list').addClass('hidden');
+  }
+  
+});
+  
+        
     }
     ;
 
