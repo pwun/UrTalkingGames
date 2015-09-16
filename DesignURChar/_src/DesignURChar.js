914e21f5e25f2a70ec23197f38f73d01a7b47851
FelixSellmair <felix.sellmair@gmail.com>
2015-09-16 14:00:50 +0200

    DesignURChar_v1.1

---
+++ DesignURChar/_src/DesignURChar.js
@@ -0,0 +1,12 @@
+var DesignURChar = {
+    init: function() {
+        "use strict";
+        var consoleController = DesignURChar.ConsoleController;
+        console.log(consoleController);
+        consoleController.init();
+        var canvasController = DesignURChar.CanvasController.init(consoleController);
+        DesignURChar.UIController.init(canvasController);
+        
+        DesignURChar.DatabaseController.init();
+    }
+};
